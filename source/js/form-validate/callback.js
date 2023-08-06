fea3536599c957643c3c2503cbd4bd98d306f6e5
qdar <qdardesign@gmail.com>
2023-08-06 21:35:43 +0300

    new lang

--- source/js/form-validate/callback.js
+++ source/js/form-validate/callback.js
@@ -1,7 +1,44 @@
-
 const baseSuccessCallback = (event) => {
   event.preventDefault();
-  // В данном колбеке бэкендер, либо разработчик при необходимости будет писать запрос на отправку формы на сервер и обрабатывать возможные ошибки или успешную отправку формы на сервер
+
+  // fetch(document.forms.user.action, {
+  //   method: 'POST',
+  //   body: JSON.stringify(new FormData(document.forms.user)),
+  //   headers: {
+  //     'Content-type': 'application/json; charset=UTF-8',
+  //   },
+  // })
+  // .then((response) => response.json())
+  // .then((data) => {
+  //   console.log(data)
+  // })
+
+  let response = fetch(document.forms.user.action, {
+    method: 'post',
+    body: new FormData(document.forms.user)
+  });
+
+  if (response.ok) {
+    let result = response.json();
+    document.forms.user.querySelectorAll('.error').forEach(el => {
+      el.textContent = '';
+    })
+    if (result['result'] === 'error') {
+      const errors = result['error'];
+      for (const [key, value] of Object.entries(errors)) {
+        document.forms.user.querySelector(`[name="${key}"]`).nextElementSibling.textContent = value;
+      }
+    } else {
+      document.forms.user.reset();
+    }
+  }
+
+  let form = document.querySelector('.intouch__form');
+  form.classList.add("success");
+
+  // const success = document.querySelector('.modal-success');
+  // success.classList.add("is-active");
+
 };
 
 const baseErrorCallback = (event) => {
